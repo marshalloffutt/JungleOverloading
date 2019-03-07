32e79b56e3a30dfd6a7cf9d0d4a11594a45247df
Marshall Offutt <marshalloffutt@gmail.com>
2019-03-06 20:31:35 -0600

    finishes the exercise

---
+++ JungleOverloading/Company.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JungleOverloading
+{
+    class Company
+    {
+        public string Name { get; }
+
+        public List<Employee> jungleBoogie = new List<Employee>();
+
+        public void addNewEmployee(Employee newEmployee)
+        {
+            jungleBoogie.Add(newEmployee);
+        }
+
+        public void removeEmployee(Employee employee)
+        {
+            jungleBoogie.Remove(employee);
+        }
+
+        public Company(string name)
+        {
+            Name = name;
+        }
+
+        public void listFolks()
+        {
+            foreach (var employee in jungleBoogie)
+            {
+                Console.WriteLine($"{employee.FirstName}, ");
+            }
+        }
+
+    }
+}
--- JungleOverloading/Program.cs
+++ JungleOverloading/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace JungleOverloading
 {
@@ -6,7 +7,28 @@ namespace JungleOverloading
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Instantiate a new company
+            var jungleBoogie = new Company("Jungle Boogie");
+
+            //Instantiate some new employees
+            var bagheera = new Employee("Bagheera", "Pantherson");
+            var sherekhan = new Employee("Shere Kahn", "Lionson");
+            var kaa = new Employee("Kaa", "Snakerson");
+            var baloo = new Employee("Baloo", "Bearperson");
+
+            //Add these employees to the list
+            jungleBoogie.addNewEmployee(bagheera);
+            jungleBoogie.addNewEmployee(sherekhan);
+            jungleBoogie.addNewEmployee(kaa);
+            jungleBoogie.addNewEmployee(baloo);
+
+            //Baloo goes out to lunch
+            baloo.eat();
+            baloo.eat("fish");
+            baloo.eat(jungleBoogie.jungleBoogie);
+            baloo.eat("soup", jungleBoogie.jungleBoogie);
+
+            Console.ReadKey();
         }
     }
 }
